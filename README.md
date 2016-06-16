28c909450f2b95086650f878fb39860cd4d4d79c
mailhap <mailhap@qq.com>
2016-06-17 00:50:16 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # real-interval
-不会被机器休眠干扰的间隔计时器
+
+A interval timer never sleep.
+
+## Install
+
+Run terminal, and type code as below:
+
+```
+npm install real-interval
+```
+
+## Usage
+
+```
+var Interval = require('real-interval');
+
+var timer = new Interval(function(pass){
+    console.log(pass);
+}, 1000);
+
+timer.stop();
+```
---
+++ src/index.js
@@ -0,0 +1,43 @@
+function interval(callback, intv, total) {
+    var self = this,
+        st = new Date().getTime(),
+        count = 0,
+        excuteCount = 0,
+        span,
+        fn;
+
+    if(!total){
+        total = 0;
+    };
+
+    fn = function() {
+        span = new Date().getTime() - st;
+        count = Math.floor(span / intv);
+        span = intv - (span % intv);
+
+        self._timer = setTimeout(function() {
+            if (self._stop) return;
+            count++;
+            excuteCount++;
+            fn();
+            if(false === callback.call(self, 
+                count, 
+                total - count, 
+                count - excuteCount
+                )
+            ){
+                self.stop();
+            };
+        }, span);
+    };
+
+    this.stop = function() {
+        this._stop = true;
+    };
+
+    fn(0);
+
+    return self;
+};
+
+return interval;
